520ca1e262d15b680b656a4b2f22018654642c07
elijahflanagan <elijahflanagan@my.tridenttech.edu>
2024-11-06 19:39:02 -0500

    Update lab7-2.cpp

--- lab7-2.cpp
+++ lab7-2.cpp
@@ -1,8 +1,8 @@
-//Lab7-2.cpp - Displays the number of years required
-//for a company's sales to reach at least $150,000
-//using a 5.5% annual growth rate. Also displays
-//the sales at that time.
-//Created/revised by <your name> on <current date>
+// Lab7-2.cpp - Displays the number of years required
+// for a company's sales to reach at least $150,000
+// using a 5.5% annual growth rate. Also displays
+// the sales at that time.
+// Created/revised by Elijah Flanagan on 11/05/2024
 
 #include <iostream>
 #include <iomanip>
@@ -10,5 +10,24 @@ using namespace std;
 
 int main()
 {
+    double growthRate = 0.055;
+    double sales = 0.0;
+    double salesTarget = 150000.0;
+    int years = 0;
+    double annualIncrease = 0.0;
+
+    cout << "Current year's sales: ";
+    cin >> sales;
+
+    while (sales < salesTarget)
+    {
+        annualIncrease = sales * growthRate;
+        sales += annualIncrease;
+        years += 1;
+    }
+
+    cout << fixed << setprecision(2);
+    cout << "Sales after " << years << " years: $" << sales << endl;
+
     return 0;
-} //end of main function
+} // end of main function
