278eab901d6ed9fdf86aa208d8c3d66c40705b51
elijahflanagan <elijahflanagan@my.tridenttech.edu>
2024-11-06 19:38:03 -0500

    Update hw7-16.cpp

--- hw7-16.cpp
+++ hw7-16.cpp
@@ -1,26 +1,29 @@
-//hw7-16.cpp - displays three tip amounts
-//Created/revised by <your name> on <current date>
+// hw7-16.cpp - displays three tip amounts
+// Created/revised by Elijah Flanagan on 11/05/2024
 
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 using namespace std;
 
-int main()
-{
-	double bill = 0.0;
-	double tip = 0.0;
+int main() {
+  double bill = 0.0;
+  double tip = 0.0;
 
-	cout << "Bill amount: ";
-	cin >> bill;
+  cout << "Bill amount: ";
+  cin >> bill;
 
-	cout << fixed;
-	for (double rate = 0.1; rate <= 0.2; rate += 0.05)
-	{
-		tip = bill * rate;
-		cout << setprecision(0);
-		cout << rate * 100 << "% tip: ";
-		cout << setprecision(2);
-		cout << "$" << tip << endl;
-	}   // end for
-	return 0;
-}	//end of main function
+  cout << fixed;
+  double rate = 0.1;  // Initialize rate before the loop
+  while (rate <= 0.2) // Loop until rate is greater than 0.2
+  {
+    tip = bill * rate;
+    cout << setprecision(0);
+    cout << rate * 100 << "% tip: ";
+    cout << setprecision(2);
+    cout << "$" << tip << endl;
+
+    rate += 0.05; // Increment rate within the loop
+  }               // end while
+
+  return 0;
+} // end of main function
