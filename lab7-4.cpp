33b32cd5f3c08db840e81c25b841b7dd46513706
elijahflanagan <elijahflanagan@my.tridenttech.edu>
2024-11-06 19:40:20 -0500

    Update lab7-4.cpp

--- lab7-4.cpp
+++ lab7-4.cpp
@@ -1,12 +1,37 @@
-//Lab7-4.cpp - displays the average electric bill
-//Created/revised by <your name> on <current date>
+// Lab7-4.cpp - displays the average electric bill
+// Created/revised by Elijah Flanagan on 11/05/2024
 
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
+		double avgBill = 0.0;
+		double bill = 0.0;
+		double totalBills = 0.0;
+		int months = 0;
 
-	return 0;
-}	//end of main function
+		cout << fixed << setprecision(2);
+
+		while (true) {
+				cout << "Bill for month " << months + 1 << ": ";
+				cin >> bill;
+
+				if (bill >= 0.0) {
+						totalBills += bill;
+						months += 1;
+				} else {
+						if (months == 0) {
+								cout << "No bill amount entered." << endl;
+						}
+						break;  // Exit the loop when a negative bill is entered
+				}
+		}
+
+		if (months > 0) {
+				avgBill = totalBills / months;
+				cout << "Average electric bill for " << months << " months: $" << avgBill << endl;
+		}
+
+		return 0;
+}
